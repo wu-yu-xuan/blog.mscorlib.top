e4b196cd4f1071bb5cdd15b1dff078468637a021
吴聿轩 <929003281@qq.com>
2018-11-01 14:57:17 +0800

    yarn add react-transition-group and TabRoute

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,29 +1,22 @@
 import * as React from 'react';
-import * as style from './style.scss';
-import { LocaleProvider, Tabs } from 'antd';
+import { LocaleProvider } from 'antd';
 import zhCN from 'antd/lib/locale-provider/zh_CN';
-import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import MarkdownView from './MarkdownView';
 import RichtextView from './RichtextView';
+import TabRoute, { TabRouteElement } from './TabRoute';
 
-const { TabPane } = Tabs;
 
 export default class App extends React.Component {
+    private tabRouteComponents: TabRouteElement[] = [
+        { path: '/richtext', tabName: 'Richtext', Component: RichtextView },
+        { path: '/markdown', tabName: 'Markdown', Component: MarkdownView }
+    ];
     public render() {
         return (
             <LocaleProvider locale={zhCN}>
                 <BrowserRouter>
-                    <>
-                        <Tabs>
-                            <TabPane tab={<NavLink to="richtext" className={style.tab}>RichText</NavLink>} key="1" />
-                            <TabPane tab={<NavLink to="markdown" className={style.tab}>Markdown</NavLink>} key="2" />
-                        </Tabs>
-                        <Switch>
-                            <Route path="/richtext" component={RichtextView} />
-                            <Route path="/markdown" component={MarkdownView} />
-                            <Route path="/" component={RichtextView} />
-                        </Switch>
-                    </>
+                    <TabRoute components={this.tabRouteComponents} />
                 </BrowserRouter>
             </LocaleProvider>
         )
---
+++ src/views/TabRoute.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { withRouter, RouteComponentProps } from 'react-router';
+import { Tabs } from 'antd';
+import { NavLink } from 'react-router-dom';
+import * as style from './style.scss';
+import { CSSTransition } from 'react-transition-group';
+
+const { TabPane } = Tabs;
+
+export interface TabRouteElement {
+    path: string;
+    tabName: string;
+    Component: React.ComponentType
+}
+
+export interface TabRouteProps {
+    components: TabRouteElement[];
+}
+
+export default withRouter(class TabRoute extends React.Component<RouteComponentProps<{}> & TabRouteProps>{
+    private findMatchIndex = () => {
+        const { components, location } = this.props;
+        const result = components.findIndex(({ path }) => !!location.pathname.match(new RegExp(`^${path}`, 'i')));
+        if (result === -1) {
+            return 0;
+        }
+        return result;
+    }
+    public render() {
+        const { components } = this.props;
+        const matchIndex = this.findMatchIndex();
+        console.log(matchIndex);
+        return (
+            <>
+                <Tabs activeKey={matchIndex.toString()}>
+                    {components.map(({ tabName, path }, index) => tabName && (
+                        <TabPane tab={<NavLink to={path} className={style.tab}>{tabName}</NavLink>} key={index.toString()} />
+                    ))}
+                </Tabs>
+                <div className={style.tabRouteContentWrapper}>
+                    {components.map(({ Component }, index) => Component && (
+                        <CSSTransition in={index === matchIndex} timeout={300} key={index} mountOnEnter={true} classNames={{
+                            appear: 'appear',
+                            appearActive: 'appearActive',
+                            enterActive: style.enterActive,
+                            enter: style.enter,
+                            enterDone: 'enterDone',
+                            exit: 'exit',
+                            exitActive: style.exitActive,
+                            exitDone: style.exitDone
+                        }}>
+                            <div className={style.tabRouteContent}>
+                                <Component />
+                            </div>
+                        </CSSTransition>
+                    ))}
+                </div>
+            </>
+        )
+    }
+})
