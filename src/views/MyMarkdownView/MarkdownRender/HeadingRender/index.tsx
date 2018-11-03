31ce485f38535cd05d8096e78653aa6a1354903c
吴聿轩 <929003281@qq.com>
2018-11-03 22:17:59 +0800

    HeadingRender

--- src/views/MyMarkdownView/MarkdownRender/HeadingRender/index.tsx
+++ src/views/MyMarkdownView/MarkdownRender/HeadingRender/index.tsx
@@ -8,12 +8,15 @@ import { Icon } from 'antd';
 export default class HeadingRender extends React.PureComponent<Tokens.Heading>{
   public render() {
     const { depth, text } = this.props;
-    const Tag = `h${depth}`
+    const Tag = `h${depth}`;
+    const idMatch = text.match(/(.+) +{#(.+)}/);
+    const id = idMatch ? idMatch[2] : text;
+    const txt = idMatch ? idMatch[1] : text;
     return (
       <BlockWrapper>
-        <Tag className={style[Tag]} id={text}>
-          {renderText(text)}
-          <a href={`#${text}`} className={style.link}>
+        <Tag className={style[Tag]} id={id}>
+          {renderText(txt)}
+          <a href={`#${id}`} className={style.link}>
             <Icon type="link" />
           </a>
         </Tag>
