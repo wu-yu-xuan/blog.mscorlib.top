2c410cc35b9b283b643085b8a9f02f9cef752d39
吴聿轩 <929003281@qq.com>
2018-11-13 13:08:02 +0800

    sortBy

--- src/views/BlogView/BlogList/index.tsx
+++ src/views/BlogView/BlogList/index.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react';
 import * as style from './style.scss';
 import BlogListItem from './BlogListItem';
+import { Radio } from 'antd';
+import { RadioChangeEvent } from 'antd/lib/radio';
+
+const { Group, Button } = Radio;
+
+enum Sort {
+  birthTime,
+  modifyTime
+}
 
 export interface BlogSummary {
   title: string;
@@ -11,15 +20,18 @@ export interface BlogSummary {
 
 interface BlogListState {
   blogSummarys: BlogSummary[];
+  sortBy: Sort
 }
 
 export default class BlogList extends React.Component<{}, BlogListState> {
   constructor(props: {}) {
     super(props);
     this.state = {
-      blogSummarys: []
+      blogSummarys: [],
+      sortBy: Sort.modifyTime
     };
   }
+  private handleRadioChange = (e: RadioChangeEvent) => this.setState({ sortBy: e.target.value })
   public async componentDidMount() {
     document.title = `wyx's blog`;
     const response = await fetch('/markdown/list.json');
@@ -38,10 +50,24 @@ export default class BlogList extends React.Component<{}, BlogListState> {
     this.setState({ blogSummarys });
   }
   public render() {
+    const { blogSummarys, sortBy } = this.state;
     return (
-      <div className={style.blogListContainer} >
-        {this.state.blogSummarys.sort((a, b) => b.modifyTime - a.modifyTime).map((blogSummary, key) => <BlogListItem {...blogSummary} key={key} />)}
-      </div>
+      <>
+        <div>
+          <span className={style.radioText}>sort by:</span>
+          <Group value={sortBy} onChange={this.handleRadioChange}>
+            <Button value={Sort.modifyTime}>修改时间</Button>
+            <Button value={Sort.birthTime}>发布时间</Button>
+          </Group>
+        </div>
+        <div className={style.blogListContainer} >
+          {
+            blogSummarys
+              .sort((a, b) => sortBy === Sort.modifyTime ? b.modifyTime - a.modifyTime : b.birthTime - a.birthTime)
+              .map((blogSummary, key) => <BlogListItem {...blogSummary} key={key} />)
+          }
+        </div>
+      </>
     )
   }
 }
