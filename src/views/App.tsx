0062c738e6a29d5255b3707e31711a05eef9f032
吴聿轩 <929003281@qq.com>
2019-03-25 15:45:20 +0800

    functionalize App

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -7,21 +7,20 @@ import Corner from './Corner';
 import BlogView from './BlogView';
 import MarkdownView from './MarkdownView';
 
-export default class App extends React.Component {
-  private tabRouteComponents: TabRouteElement[] = [
-    { path: '/blog', tabName: 'Blog', title: 'wyx\'s blog', Component: BlogView },
-    { path: '/markdonwn', tabName: 'Markdown', title: 'wyx\'s markdown playground', Component: MarkdownView }
-  ];
-  public render() {
-    return (
-      <LocaleProvider locale={zhCN}>
-        <BrowserRouter>
-          <>
-            <Corner />
-            <TabRoute components={this.tabRouteComponents} />
-          </>
-        </BrowserRouter>
-      </LocaleProvider>
-    )
-  }
+const tabRouteComponents: TabRouteElement[] = [
+  { path: '/blog', tabName: 'Blog', title: 'wyx\'s blog', Component: BlogView },
+  { path: '/markdonwn', tabName: 'Markdown', title: 'wyx\'s markdown playground', Component: MarkdownView }
+];
+
+export default function App() {
+  return (
+    <LocaleProvider locale={zhCN}>
+      <BrowserRouter>
+        <>
+          <Corner />
+          <TabRoute components={tabRouteComponents} />
+        </>
+      </BrowserRouter>
+    </LocaleProvider>
+  )
 }
