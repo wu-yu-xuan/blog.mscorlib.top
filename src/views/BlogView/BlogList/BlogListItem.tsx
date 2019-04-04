10ec475beaf438c5cef51bec83305d8d27fda4fd
吴聿轩 <929003281@qq.com>
2019-04-04 11:35:14 +0800

    functionalize BlogListItem

--- src/views/BlogView/BlogList/BlogListItem.tsx
+++ src/views/BlogView/BlogList/BlogListItem.tsx
@@ -3,23 +3,21 @@ import * as React from 'react';
 import * as style from './style.scss';
 import { Link } from 'react-router-dom';
 
-export default class BlogListItem extends React.PureComponent<BlogSummary>{
-  private formatTime = (time: number) => {
-    const date = new Date(time);
-    return [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('-');
-  }
-  public render() {
-    const { title, birthTime, modifyTime } = this.props;
-    return (
-      <div className={style.blogListItemContainer}>
-        <Link to={`/blog/${title.replace(/ /g, '-')}`} className={style.title} title={title}>
-          {title}
-        </Link>
-        <div className={style.footer}>
-          <span className={style.text}>发布于 {this.formatTime(birthTime)}</span>
-          <span className={style.text}>修改于 {this.formatTime(modifyTime)}</span>
-        </div>
-      </div>
-    )
-  }
+function formatTime(time: number) {
+  const date = new Date(time);
+  return [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('-');
 }
+
+export default React.memo(function BlogListItem({ title, birthTime, modifyTime }: BlogSummary) {
+  return (
+    <div className={style.blogListItemContainer}>
+      <Link to={`/blog/${title.replace(/ /g, '-')}`} className={style.title} title={title}>
+        {title}
+      </Link>
+      <div className={style.footer}>
+        <span className={style.text}>发布于 {formatTime(birthTime)}</span>
+        <span className={style.text}>修改于 {formatTime(modifyTime)}</span>
+      </div>
+    </div>
+  )
+})
