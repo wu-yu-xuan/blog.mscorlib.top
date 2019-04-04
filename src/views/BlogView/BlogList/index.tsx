ef982db17dc4f769e00cd5698523986e26f55b32
吴聿轩 <929003281@qq.com>
2019-04-04 12:29:19 +0800

    functionalize BlogList

--- src/views/BlogView/BlogList/index.tsx
+++ src/views/BlogView/BlogList/index.tsx
@@ -18,55 +18,52 @@ export interface BlogSummary {
   birthTime: number;
 }
 
-interface BlogListState {
-  blogSummarys: BlogSummary[];
-  sortBy: Sort
-}
+export default React.memo(function BlogList() {
+  const blogSummarys = useBlogSummarys();
+  const [sortBy, handleRadioChange] = useSortBy();
 
-export default class BlogList extends React.Component<{}, BlogListState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      blogSummarys: [],
-      sortBy: Sort.modifyTime
-    };
-  }
-  private handleRadioChange = (e: RadioChangeEvent) => this.setState({ sortBy: e.target.value })
-  public async componentDidMount() {
-    const response = await fetch('/markdown/list.json');
-    if (!response.ok) {
-      this.setState({
-        blogSummarys: [{
+  return (
+    <>
+      <div>
+        <span className={style.radioText}>sort by:</span>
+        <Group value={sortBy} onChange={handleRadioChange}>
+          <Button value={Sort.modifyTime}>修改时间</Button>
+          <Button value={Sort.birthTime}>发布时间</Button>
+        </Group>
+      </div>
+      <div className={style.blogListContainer} >
+        {
+          blogSummarys
+            .sort((a, b) => sortBy === Sort.modifyTime ? b.modifyTime - a.modifyTime : b.birthTime - a.birthTime)
+            .map((blogSummary, key) => <BlogListItem {...blogSummary} key={key} />)
+        }
+      </div>
+    </>
+  )
+})
+
+function useBlogSummarys() {
+  const [blogSummarys, setBlogSummarys] = React.useState<BlogSummary[]>([]);
+  React.useEffect(() => {
+    (async () => {
+      const response = await fetch('/markdown/list.json');
+      if (!response.ok) {
+        setBlogSummarys([{
           title: 'unable to connect to the server',
           path: '/blog/',
           birthTime: 0,
           modifyTime: 0
-        }]
-      });
-      return;
-    }
-    const blogSummarys = await response.json();
-    this.setState({ blogSummarys });
-  }
-  public render() {
-    const { blogSummarys, sortBy } = this.state;
-    return (
-      <>
-        <div>
-          <span className={style.radioText}>sort by:</span>
-          <Group value={sortBy} onChange={this.handleRadioChange}>
-            <Button value={Sort.modifyTime}>修改时间</Button>
-            <Button value={Sort.birthTime}>发布时间</Button>
-          </Group>
-        </div>
-        <div className={style.blogListContainer} >
-          {
-            blogSummarys
-              .sort((a, b) => sortBy === Sort.modifyTime ? b.modifyTime - a.modifyTime : b.birthTime - a.birthTime)
-              .map((blogSummary, key) => <BlogListItem {...blogSummary} key={key} />)
-          }
-        </div>
-      </>
-    )
-  }
+        }]);
+        return;
+      }
+      setBlogSummarys(await response.json());
+    })();
+  }, []);
+  return blogSummarys;
+}
+
+function useSortBy(): [Sort, (e: RadioChangeEvent) => void] {
+  const [sortBy, setSortBy] = React.useState<Sort>(Sort.modifyTime);
+  const handleRadioChange = React.useCallback((e: RadioChangeEvent) => setSortBy(e.target.value), []);
+  return [sortBy, handleRadioChange];
 }
