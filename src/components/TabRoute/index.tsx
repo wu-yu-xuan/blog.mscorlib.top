4e61a07528a6e2e6eba1e052fff7086840b54f40
吴聿轩 <929003281@qq.com>
2019-04-07 15:51:44 +0800

    functionalize TabRoute

--- src/components/TabRoute/index.tsx
+++ src/components/TabRoute/index.tsx
@@ -17,35 +17,22 @@ export interface TabRouteProps {
   components: TabRouteElement[];
 }
 
-export default withRouter(class TabRoute extends React.Component<RouteComponentProps<{}> & TabRouteProps>{
-  private findMatchIndex = () => {
-    const { components, location } = this.props;
-    const result = components.findIndex(({ path }) => !!location.pathname.match(new RegExp(`^${path}`, 'i')));
-    if (result === -1) {
-      return 0;
-    }
-    return result;
-  }
-  private handleTabsChange = (activeKey: string) => {
-    const { title } = this.props.components[parseInt(activeKey, 10)];
-    document.title = title;
-  }
-  public componentDidMount() {
-    document.title = this.props.components[this.findMatchIndex()].title;
-  }
-  public render() {
-    const { components } = this.props;
-    const matchIndex = this.findMatchIndex();
-    return (
-      <div className={style.tabContainer}>
-        <Tabs activeKey={matchIndex.toString()} className={style.tabs} onChange={this.handleTabsChange}>
-          {components.map(({ tabName, Component, path }, index) => tabName && (
-            <TabPane tab={<NavLink to={path} className={style.tab}>{tabName}</NavLink>} key={index.toString()} >
-              <Component />
-            </TabPane>
-          ))}
-        </Tabs>
-      </div>
-    )
-  }
+export default withRouter(function TabRoute({ location, components }: TabRouteProps & RouteComponentProps) {
+  const [activeKey, setActiveKey] = React.useState(() => (
+    Math.max(0, components.findIndex(({ path }) => !!location.pathname.match(new RegExp(`^${path}`, 'i')))).toString()
+  ));
+  React.useEffect(() => {
+    document.title = components[activeKey].title;
+  }, [activeKey]);
+  return (
+    <div className={style.tabContainer}>
+      <Tabs activeKey={activeKey} className={style.tabs} onChange={setActiveKey}>
+        {components.map(({ tabName, Component, path }, index) => tabName && (
+          <TabPane tab={<NavLink to={path} className={style.tab}>{tabName}</NavLink>} key={index.toString()}>
+            <Component />
+          </TabPane>
+        ))}
+      </Tabs>
+    </div>
+  )
 })
