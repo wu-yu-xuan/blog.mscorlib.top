3859473ddfec073c11e30ef9b83fda31e0f34346
吴聿轩 <929003281@qq.com>
2019-05-03 13:03:13 +0800

    functionalize BlogView

--- src/views/BlogView/index.tsx
+++ src/views/BlogView/index.tsx
@@ -3,13 +3,11 @@ import { Switch, Route } from 'react-router';
 import Article from './Article';
 import BlogList from './BlogList';
 
-export default class BlogView extends React.Component {
-  public render() {
-    return (
-      <Switch>
-        <Route path='/blog/:title' component={Article} />
-        <Route component={BlogList} />
-      </Switch>
-    )
-  }
+export default function BlogView() {
+  return (
+    <Switch>
+      <Route path='/blog/:title' component={Article} />
+      <Route component={BlogList} />
+    </Switch>
+  )
 }
