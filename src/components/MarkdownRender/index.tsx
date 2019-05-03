0eaa083250cad3ec65c2cb5a025182ec15ee53dc
吴聿轩 <929003281@qq.com>
2019-05-03 13:26:59 +0800

    functionalize MarkdownRender

--- src/components/MarkdownRender/index.tsx
+++ src/components/MarkdownRender/index.tsx
@@ -12,8 +12,7 @@ import ListRender, { ListRenderProps } from './ListRender';
 import BlockquoteRender from './BlockquoteRender';
 import TableRender from './TableRender';
 
-interface MarkdownRenderProps {
-  source: string;
+interface MarkdownRenders {
   heading?: React.ComponentType<Tokens.Heading>;
   hr?: React.ComponentType;
   paragraph?: React.ComponentType<Tokens.Paragraph>;
@@ -24,65 +23,84 @@ interface MarkdownRenderProps {
   list?: React.ComponentType<ListRenderProps>;
   blockquote?: React.ComponentType<Tokens.BlockquoteStart>;
   table?: React.ComponentType<Tokens.Table>;
+}
+
+interface MarkdownRenderProps extends MarkdownRenders {
+  source: string;
   onDidUpdate?(): void;
 }
 
-export default class MarkdownRender extends React.PureComponent<MarkdownRenderProps>{
-  public static defaultProps: MarkdownRenderProps = {
-    source: '',
-    heading: HeadingRender,
-    hr: HrRender,
-    paragraph: ParagraphRender,
-    text: TextRender,
-    html: HtmlRender,
-    code: CodeRender,
-    list_item: ListItemRender,
-    list: ListRender,
-    blockquote: BlockquoteRender,
-    table: TableRender
-  }
-  private renderComponents = (lexList: marked.TokensList | marked.Token[]): React.ReactNode => {
-    let depth = 0;
-    return lexList.map((token, key) => {
-      if (token.type.endsWith('_end')) {
-        depth--;
-      }
-      if (token.type.endsWith('_start')) {
-        depth++;
-      }
-      if (depth === 1 && token.type.endsWith('_start')) {
-        const startName = token.type;
-        const endName = token.type.replace('start', 'end');
-        let deep = 1;
-        const endIndex = key + 1 + lexList.slice(key + 1).findIndex(val => {
-          if (val.type === startName) {
-            deep++;
-          }
-          if (val.type === endName) {
-            deep--;
-          }
-          return !deep;
-        });
-        const Component: React.ComponentType = this.props[token.type.replace('_start', '')];
-        return Component && (
-          <Component {...token} key={key}>
-            {this.renderComponents(lexList.slice(key + 1, endIndex))}
-          </Component>
-        )
-      }
-      if (depth) {
-        return null;
-      }
-      const SimpleComponent: React.ComponentType = this.props[token.type];
-      return SimpleComponent && <SimpleComponent {...token} key={key} />;
-    });
-  };
-  public componentDidUpdate() {
-    this.props.onDidUpdate && this.props.onDidUpdate();
-  }
-  public render() {
-    const { source } = this.props;
+export default function MarkdownRender({
+  source = '',
+  heading = HeadingRender,
+  hr = HrRender,
+  paragraph = ParagraphRender,
+  text = TextRender,
+  html = HtmlRender,
+  code = CodeRender,
+  list_item = ListItemRender,
+  list = ListRender,
+  blockquote = BlockquoteRender,
+  table = TableRender,
+  onDidUpdate
+}: MarkdownRenderProps) {
+  const [result, setResult] = React.useState<React.ReactNode>([]);
+  React.useEffect(() => {
     const lexList = lexer(source);
-    return <div className={style.markdownContainer}>{this.renderComponents(lexList)}</div>
-  }
+    setResult(renderComponents(lexList, {
+      heading,
+      hr,
+      paragraph,
+      text,
+      html,
+      code,
+      list_item,
+      list,
+      blockquote,
+      table
+    }));
+  }, [source]);
+  React.useEffect(() => {
+    if ((result as React.ReactNode[]).length && onDidUpdate) {
+      onDidUpdate();
+    }
+  }, [result]);
+  return <div className={style.markdownContainer}>{result}</div>
+}
+
+function renderComponents(lexList: marked.TokensList | marked.Token[], props: MarkdownRenders): React.ReactNode {
+  let depth = 0;
+  return lexList.map((token, key) => {
+    if (token.type.endsWith('_end')) {
+      depth--;
+    }
+    if (token.type.endsWith('_start')) {
+      depth++;
+    }
+    if (depth === 1 && token.type.endsWith('_start')) {
+      const startName = token.type;
+      const endName = token.type.replace('start', 'end');
+      let deep = 1;
+      const endIndex = key + 1 + lexList.slice(key + 1).findIndex(val => {
+        if (val.type === startName) {
+          deep++;
+        }
+        if (val.type === endName) {
+          deep--;
+        }
+        return !deep;
+      });
+      const Component: React.ComponentType = props[token.type.replace('_start', '')];
+      return Component && (
+        <Component {...token} key={key}>
+          {renderComponents(lexList.slice(key + 1, endIndex), props)}
+        </Component>
+      )
+    }
+    if (depth) {
+      return null;
+    }
+    const SimpleComponent: React.ComponentType = props[token.type];
+    return SimpleComponent && <SimpleComponent {...token} key={key} />;
+  });
 }
