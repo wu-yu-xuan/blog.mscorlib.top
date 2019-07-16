6226269f69beb5ff3c137eb5690204694a5a4ad8
wuyuxuan.wyx <wuyuxuan.wyx@alibaba-inc.com>
2019-07-16 15:43:42 +0800

    手贱升包

--- src/components/TabRoute/useRouter.ts
+++ src/components/TabRoute/useRouter.ts
@@ -1,13 +1,17 @@
-import { useContext } from 'react'
+import { useContext } from 'react';
 import { RouteComponentProps, StaticContext } from 'react-router';
 import * as H from 'history';
 // @ts-ignore
-import { __RouterContext as RouterContext } from 'react-router'
+import { __RouterContext as RouterContext } from 'react-router';
 
 /**
  * @see https://github.com/ReactTraining/react-router/pull/6453
  * @see https://github.com/ReactTraining/react-router/issues/6430
  */
-export function useRouter<Params extends { [K in keyof Params]?: string } = {}, C extends StaticContext = StaticContext, S = H.LocationState>() {
+export function useRouter<
+  Params extends { [K in keyof Params]?: string } = {},
+  C extends StaticContext = StaticContext,
+  S = H.LocationState
+>() {
   return useContext(RouterContext) as RouteComponentProps<Params, C, S>;
 }
