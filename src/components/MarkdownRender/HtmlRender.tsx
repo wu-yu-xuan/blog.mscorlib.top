88eb4ca0f6db3cd64755c709f985fd268cd21769
wuyuxuan.wyx <wuyuxuan.wyx@alibaba-inc.com>
2019-08-03 19:03:28 +0800

    2019.8.3

--- src/components/MarkdownRender/HtmlRender.tsx
+++ src/components/MarkdownRender/HtmlRender.tsx
@@ -5,19 +5,28 @@ import RegJSXMap, { IRegJSXMap } from './RegJSXMap';
 
 const blockTags = ['div', 'iframe', 'p'];
 
-const map: IRegJSXMap = new Map<RegExp, (reg: RegExpMatchArray) => JSX.Element>([
-  [/^<(.+?)\s/, (reg) => {
-    if (blockTags.includes(reg[1])) {
-      return (
-        <BlockWrapper>
-          <div dangerouslySetInnerHTML={{ __html: reg.input }} />
-        </BlockWrapper>
-      );
-    } else {
-      return <span dangerouslySetInnerHTML={{ __html: reg.input }} />
-    }
-  }], [/[\w\W]*/, ({ input }) => <span dangerouslySetInnerHTML={{ __html: input }} />]
-]);
+const map: IRegJSXMap = new Map<RegExp, (reg: RegExpMatchArray) => JSX.Element>(
+  [
+    [
+      /^<(.+?)(\s|>)/,
+      reg => {
+        if (blockTags.includes(reg[1])) {
+          return (
+            <BlockWrapper>
+              <div dangerouslySetInnerHTML={{ __html: reg.input }} />
+            </BlockWrapper>
+          );
+        } else {
+          return <span dangerouslySetInnerHTML={{ __html: reg.input }} />;
+        }
+      }
+    ],
+    [
+      /[\w\W]*/,
+      ({ input }) => <span dangerouslySetInnerHTML={{ __html: input }} />
+    ]
+  ]
+);
 
 export default React.memo(function HtmlRender({ text }: Tokens.HTML) {
   return RegJSXMap(text, map);
