1920daae19fec4215bb2923e294442515102fdc2
吴聿轩 <929003281@qq.com>
2020-01-30 12:24:47 +0800

    yarn remove dotenv

--- config/paths.js
+++ config/paths.js
@@ -40,7 +40,6 @@ function getServedPath(appPackageJson) {
 
 // config after eject: we're in ./config/
 module.exports = {
-  dotenv: resolveApp('.env'),
   appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
