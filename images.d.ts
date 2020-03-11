81e74cc17d7cd2d8292d6d45483b0c08fcf9854d
吴聿轩 <929003281@qq.com>
2020-03-11 10:59:21 +0800

    把两个声明拆开来才能让webpack通过编译...

--- images.d.ts
+++ images.d.ts
@@ -6,9 +6,3 @@ declare module '*.gif';
 declare module '*.bmp';
 declare module '*.tiff';
 declare module '*.scss';
-
-/**
- * 加载实验性 React 声明, 转正后移除
- */
-import 'react/experimental';
-import 'react-dom/experimental';
---
+++ react-experiment.d.ts
@@ -0,0 +1,5 @@
+/**
+ * 加载实验性 React 声明, 转正后移除
+ */
+import 'react/experimental';
+import 'react-dom/experimental';
