9e61b40ff40984c80d437e5ff537bf767d601a7d
吴聿轩 <929003281@qq.com>
2020-03-12 09:48:59 +0800

    migrate to Concurrent Mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { createBlockingRoot } from 'react-dom';
+import { createRoot } from 'react-dom';
 import App from './views/App';
 import registerServiceWorker from './registerServiceWorker';
 
-createBlockingRoot(document.getElementById('root')).render(<App />);
+/**
+ * Concurrent Mode auto enable Strict Mode
+ */
+createRoot(document.getElementById('root')).render(<App />);
 registerServiceWorker();
